a0434b4fa791bf1766da6fb5122686d6a07086b5
lovehakumai <masaotomeza@gmail.com>
2024-11-23 18:33:19 +0900

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental:{
+        serverActions:true,
+    },
+    images:{
+        remotePatterns:[
+            {hostname: 'books.google.com'},
+        ]
+    }
+};
+
+export default nextConfig;
